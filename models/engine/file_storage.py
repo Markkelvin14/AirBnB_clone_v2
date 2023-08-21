41a6e0f99d9b9ab8a96cca7db8fbce44ae93bedb
Markkelvin14 <mjkelvin38@gmail.com>
2023-08-21 11:53:45 +0000

    AirBnB_clone_v2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        dicts = {}
+        if cls is None:
+            return self.__objects
+        for key, part in self.__objects.items():
+            if cls.__name__ == part.__class__.__name__:
+                dicts[key] = part
+        return dicts
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes obj from objects"""
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
